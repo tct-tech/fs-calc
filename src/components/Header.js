8eda05ee33f72d8b378b55b5fbc28f1a6447dbca
tct-tech <lemuellina1@gmail.com>
2023-08-25 15:32:14 -0700

    adjust font size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
+/**
+ * @Author: Your name
+ * @Date:   2023-04-28 11:51:34
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2023-08-25 15:30:05
+ */
 import React from 'react';
 import '../styles/styles.css';
 
 const Header = () => {
     return(
-        <div className='header'>TCT Feeds & Speeds Calculator</div>
+        <div className='header'>
+            <h1 className='headerText'>Feeds & Speeds Calculator</h1>    
+        </div>
     );
 }
 
